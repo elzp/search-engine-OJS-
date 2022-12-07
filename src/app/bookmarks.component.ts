7c36243dd504782fe56b73a76bf8398c5d4e4ed5
elzp <86740659+elzp@users.noreply.github.com>
2022-12-07 20:42:30 +0100

    Apply pipe with filtration to bookmark.component

--- src/app/bookmarks.component.ts
+++ src/app/bookmarks.component.ts
@@ -2,16 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-bookmarks',
-  templateUrl: './bookmarks.component.html'
+  templateUrl: './bookmarks.component.html',
 })
-export class BookmarksComponent  {
-
+export class BookmarksComponent {
   bookmarks = [
     'Przeprogramowani.pl',
     'overment.com',
     'smyrdek.com',
     'algosmart.pl',
-    'Przeprogramowani YouTube'
-  ]
-  
+    'Przeprogramowani YouTube',
+  ];
+  typeOfFilteredData = 'flatarray';
+  regex = new RegExp('');
+
+  updateData(data: string) {
+    this.regex = new RegExp(data);
+  }
 }
